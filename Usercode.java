1730953009e8668701dad89bac63c90854aa9860
Aleksandr <39279984+Oleksenko@users.noreply.github.com>
2021-02-08 14:44:59 +0200

    Update Usercode.java

--- Usercode.java
+++ Usercode.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Usercode implements IUsercode<Map<String, String>, Map<String, String>> {
     @java.lang.Override
     public Map<String, String> handle(Map<String, String> data) throws Exception {
-        data.put("hello", "Hello world!");
+        data.put("hello", "Hello Java!");
         return data;
     }
 }
